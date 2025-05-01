0f335bef402ade4276e543973b0df20af35ce6aa
Omar HP Windows <oayala775@gmail.com>
2025-04-30 18:58:50 -0600

    Finalización de proyecto

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//! Que sucede con 10.005 o 11,000 
+
 const estados = ["inicial", "binario", "octal", "decimal", "hexadecimal"];
 
 const f = [
@@ -8,39 +10,46 @@ const f = [
   [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4],
 ];
 
-document.querySelector("form").addEventListener("submit", function(event) {
+document.querySelector("form").addEventListener("submit", function (event) {
   event.preventDefault(); // Evita que el formulario recargue la página
 
   const numero = document.getElementById("number").value;
   let estado = 0, chr, idx;
   let resultadoDiv = document.getElementById("resultado");
-  let resultado = ""
-  
-  if (numero != ""){
+  let resultado = "";
+
+  if (numero != "") {
     for (let i = 0; i < numero.length; i++) {
       chr = numero[i];
-  
+
       if (/[a-fA-F]/.test(chr)) {
         idx = 10;
       } else if (/[0-9]/.test(chr)) {
         idx = Number(chr);
       } else {
-        resultadoDiv.innerHTML = '<p class="texto-resultado error">No se ha podido reconocer el sistema numerico debido a un carácter inválido</p>';
+        resultadoDiv.innerHTML = `<p class="texto-resultado error">No se ha podido reconocer el sistema numerico debido al carácter inválido: "${chr}"</p>`;
+        retardo(resultadoDiv);
         return;
       }
-  
+
       estado = f[estado][idx];
     }
-  
+
     resultado = '<p class="texto-resultado">El valor ingresado puede pertenecer a los siguientes sistemas numéricos:</p>';
     for (let i = estado; i < estados.length; i++) {
       resultado += `<p class="texto-resultado">• ${estados[i]}</p>`;
     }
-  
     resultadoDiv.innerHTML = resultado;
-  }else{
-    resultado = '<p class="texto-resultado">No se ha ingresado ningun número<p>'
+    retardo(resultadoDiv);
+  } else {
+    resultado = '<p class="texto-resultado error">No se ha ingresado ningun número<p>'
     resultadoDiv.innerHTML = resultado;
+    retardo(resultadoDiv);
   }
-  
 });
+
+function retardo(resultadoDiv) {
+  setTimeout(() => {
+    resultadoDiv.innerHTML = "";
+  }, 5000);
+}
